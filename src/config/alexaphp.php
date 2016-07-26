a53745021df19f11828c2addb9452b956c716f55
Simon Yousoufov <syousoufov@fuzzproductions.com>
2016-07-25 20:30:20 -0400

    Fix formatting of config file

--- src/config/alexaphp.php
+++ src/config/alexaphp.php
@@ -16,8 +16,7 @@ return [
 	| Header names for Alexa request data
 	|
     */
-	'cert_chain_url_header' => env('ALEXAPHP_CERT_CHAIN_URL_HEADER', 'SignatureCertChainUrl'),
-
+	'cert_chain_url_header'         => env('ALEXAPHP_CERT_CHAIN_URL_HEADER', 'SignatureCertChainUrl'),
 	'signature_header'              => env('ALEXAPHP_SIGNATURE_HEADER', 'Signature'),
 
 	/*
@@ -29,21 +28,13 @@ return [
 	|
     */
 	'timestamp_delay_limit_seconds' => env('ALEXAPHP_TIMESTAMP_DELAY_LIMIT_SECONDS', 150),
-
-	'expect_scheme' => env('ALEXAPHP_EXPECT_SCHEME', 'https'),
-
-	'expect_path_start_regexp' => env('ALEXAPHP_EXPECT_PATH_START_REGEXP', '/^\/echo.api/'),
-
-	'expect_port' => env('ALEXAPHP_EXPECT_PORT', 443),
-
-	'expect_host' => env('ALEXAPHP_EXPECT_HOST', 's3.amazonaws.com'),
-
-	'expect_san' => env('ALEXAPHP_EXPECT_SAN', 'echo-api.amazon.com'),
-
-	'encryption_method' => env('ALEXAPHP_ENCRYPTION_METHOD', 'sha1WithRSAEncryption'),
-
-	'application_id'   => env('ALEXAPHP_APPLICATION_ID', 'arbitrary'),
-
+	'expect_scheme'                 => env('ALEXAPHP_EXPECT_SCHEME', 'https'),
+	'expect_path_start_regexp'      => env('ALEXAPHP_EXPECT_PATH_START_REGEXP', '/^\/echo.api/'),
+	'expect_port'                   => env('ALEXAPHP_EXPECT_PORT', 443),
+	'expect_host'                   => env('ALEXAPHP_EXPECT_HOST', 's3.amazonaws.com'),
+	'expect_san'                    => env('ALEXAPHP_EXPECT_SAN', 'echo-api.amazon.com'),
+	'encryption_method'             => env('ALEXAPHP_ENCRYPTION_METHOD', 'sha1WithRSAEncryption'),
+	'application_id'                => env('ALEXAPHP_APPLICATION_ID', 'arbitrary'),
 	/*
 	|--------------------------------------------------------------------------
 	| Handlers
@@ -52,7 +43,7 @@ return [
 	| Configuration for Alexa request lifecycle handlers
 	|
     */
-	'request_handlers' => [
+	'request_handlers'              => [
 		LaunchRequest::REQUEST_TYPE       => LaunchRequest::class,
 		IntentRequest::REQUEST_TYPE       => IntentRequest::class,
 		SessionEndedRequest::REQUEST_TYPE => SessionEndedRequest::class,
